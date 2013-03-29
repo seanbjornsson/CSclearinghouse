b4446f3719e09ceca65f1a058a82360b98142958
sbjornsson <sbjornsson@colgate.edu>
2013-03-28 23:24:15 -0400

    cucumber and rspec tests complete for viewing all projects on projects homepage

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe ProjectsController do
-
+	describe 'list all movies in the data base on the homepage' do
+		it 'should pull the projects from the model' do
+			Project.should_receive(:all)
+			post :index
+		end
+	end
 end
